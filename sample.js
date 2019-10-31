edc4b75f50fa4a9bc15460cd83062c46815c499b
mchihiro <maki.chihiro1125@gmail.com>
2019-10-31 10:40:52 +0900

    script修正

--- sample.js
+++ sample.js
@@ -6,7 +6,7 @@ import FS from 'fs'
 const doRequest = (request, response) => {
   console.log(response)
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  FS.readfile('./index.html', 'utf-8', response.write('200', {'Content-type', 'text/html'}))
+  FS.readFile('./index.html', 'utf-8', response.write('200', {'Content-type', 'text/html'}))
   response.end();
 }
 

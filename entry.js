3e2c75f3dafd9ef0f303569207ceaccfe629634b
mchihiro <maki.chihiro1125@gmail.com>
2019-10-31 14:09:22 +0900

    ストラクチャー更新

--- entry.js
+++ entry.js
@@ -2,7 +2,7 @@ import HTTP from 'http'
 import FS from 'fs'
 import PUG from 'pug'
 
-const index = FS.readFileSync('./view/index.pug', 'utf-8')
+const index = FS.readFileSync('./Pug/index.pug', 'utf-8')
 
 const doRequest = (req, res) => {
   const pugPage = PUG.render(index)

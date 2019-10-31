2b5c34e4c1346b5dc1908e98a8c9c7d60d70f06c
mchihiro <maki.chihiro1125@gmail.com>
2019-10-31 13:55:26 +0900

    pug修正

--- entry.js
+++ entry.js
@@ -2,25 +2,29 @@ import HTTP from 'http'
 import FS from 'fs'
 import PUG from 'pug'
 
-const index = FS.readFileSync('./views/index.pug', 'utf-8')
+const index = FS.readFileSync('./view/index.pug', 'utf-8')
 
-// リクエスト処理
-const doRequest = (request, response) => {
-  FS.readFile(
-    './index.html',
-    'utf-8',
-    (err, data) => {
-      response.writeHead(200, { 'Content-Type': 'text/html' })
-      response.write(PUG.render(index))
-      response.end();
-    }
-  )
+const doRequest = (req, res) => {
+  const pugPage = PUG.render(index)
+  response.writeHead(200, { 'Content-Type': 'text/html' })
+  response.write(pugPage)
+  response.end();
 }
 
-// サーバの起動
-const SERVER = HTTP.createServer().listen(process.env.PORT, process.env.IP)
-// local
-// const SERVER = HTTP.createServer().listen(3005)
+const SERVER = HTTP.createServer().listen(3005)
 SERVER.on('request', doRequest)
-
 console.log('Server Running!')
+
+// const index = FS.readFileSync('./view/index.pug', 'utf-8')
+//
+// const doRequest = (request, response) => {
+//   response.writeHead(200, { 'Content-Type': 'text/html' })
+//   response.write(PUG.render(index))
+//   response.end();
+// }
+//
+// const SERVER = HTTP.createServer().listen(process.env.PORT, process.env.IP)
+// const SERVER = HTTP.createServer().listen(3005)
+// SERVER.on('request', doRequest)
+//
+// console.log('Server Running!')

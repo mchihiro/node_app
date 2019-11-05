6d560c6bc50d497b8839543ed32a1815d069984a
mchihiro <maki.chihiro1125@gmail.com>
2019-11-05 11:20:39 +0900

    Pugコンパイル

--- entry.js
+++ entry.js
@@ -1,30 +1,39 @@
 import HTTP from 'http'
 import FS from 'fs'
+import URL from 'url'
+import PATH from 'path'
 import PUG from 'pug'
 
-const index = FS.readFileSync('./Pug/index.pug', 'utf-8')
+const pugsPath = PATH.join(__dirname, 'src/')
+
+const root = {
+  '/': {
+    'main': pugsPath + 'index.pug',
+    'name': 'index.html'
+  },
+  '/contents': {
+    'main': pugsPath + 'contents.pug',
+    'name': 'contents'
+  }
+}
 
 const doRequest = (req, res) => {
-  const pugPage = PUG.render(index)
-  response.writeHead(200, { 'Content-Type': 'text/html' })
-  response.write(pugPage)
-  response.end();
+  const url_parts = URL.parse(req.url)
+
+  if (typeof root[url_parts.pathname] === 'undefined'){
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.end("<html><body><h1>NOT FOUND PAGE:" +
+    res.url + "</h1></body></html>")
+    return
+  }
+
+  const compiledFunction = PUG.compileFile(root[url_parts.pathname].main)
+
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+  res.write(compiledFunction())
+  res.end()
 }
 
 const SERVER = HTTP.createServer().listen(process.env.PORT, process.env.IP)
 SERVER.on('request', doRequest)
 console.log('Server Running!')
-
-// const index = FS.readFileSync('./view/index.pug', 'utf-8')
-//
-// const doRequest = (request, response) => {
-//   response.writeHead(200, { 'Content-Type': 'text/html' })
-//   response.write(PUG.render(index))
-//   response.end();
-// }
-//
-// const SERVER = HTTP.createServer().listen(process.env.PORT, process.env.IP)
-// const SERVER = HTTP.createServer().listen(3005)
-// SERVER.on('request', doRequest)
-//
-// console.log('Server Running!')
